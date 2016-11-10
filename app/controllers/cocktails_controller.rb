1b8a032c0fa95ab4c86f0585da1dc1c1ef0bb76b
Arthur Littmann <arthur.littmann@gmail.com>
2016-11-10 12:53:55 +0000

    home page set

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,9 +14,18 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to cocktails_path
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
+    end
+  end
+
+  def edit
+    @cocktail = Cocktail.update(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :edit
     end
   end
 
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,29 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail
   def new
     @dose = Dose.new
   end
 
   def create
-    @dose = Dose.create(params[])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
     @dose = Dose.find(params[:id])
+  end
+
+  private
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :description)
   end
 end
