5447bc46f73eee00891f140077e4d51f9daba2ff
Arthur Littmann <arthur.littmann@gmail.com>
2016-11-11 15:59:05 +0000

    ratings added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -45,7 +45,7 @@ class CocktailsController < ApplicationController
   end
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :description, :photo)
+    params.require(:cocktail).permit(:name, :description, :photo, :rating)
   end
 
   # def cocktail_photo
